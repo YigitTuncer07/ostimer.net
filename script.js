397f560ad272374687e558056cbe9de01124992c
YigitTuncer07 <yigit_tuncer03@hotmail.com>
2022-08-03 00:04:53 +0300

    Added minutes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let seconds = 0;
 let tens = 0;
+let minutes = 0;
 let pressed = false;
 let interval;
 
@@ -23,17 +24,29 @@ function startTime(){
         document.getElementById("tens").textContent = "00";
         tens = 0;
         seconds++;
-        document.getElementById("seconds").textContent = seconds;
+        if (seconds == 60){
+            document.getElementById("seconds").textContent = "00";
+            seconds =0;
+            minutes++;
+            document.getElementById("minutes").textContent = minutes;
+
+        } else {
+            document.getElementById("seconds").textContent = seconds;
+        }
 
     } else {
         document.getElementById("tens").textContent = tens;
     }    
+    
 }
 
 function reset(){
     tens = 0;
     seconds = 0;
+    minutes = 0;
     document.getElementById("tens").textContent = "00";
     document.getElementById("seconds").textContent = "00";
+    document.getElementById("minutes").textContent = "00";
+
 
 }
